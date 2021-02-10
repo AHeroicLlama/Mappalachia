cc15d23ec1c234086921a8472af257d68fa73574
AHeroicLlama <tom-kellett@hotmail.co.uk>
2021-02-10 15:55:07 +0000

    camelCase variable

--- Mappalachia/package_release.bat
+++ Mappalachia/package_release.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set releaseBuildFolder=bin\Release
-set outputfile=Mappalachia.zip
+set outputFile=Mappalachia.zip
 set prefsFile=mappalachia_prefs.ini
 
 echo Checking for Release build...
@@ -15,7 +15,7 @@ echo Removing preferences file prior to zipping...
 del %releaseBuildFolder%\%prefsFile%
 
 echo Zipping release...
-powershell Compress-Archive %releaseBuildFolder%\* %outputfile% -Force
+powershell Compress-Archive %releaseBuildFolder%\* %outputFile% -Force
 
 echo done.
 PAUSE
