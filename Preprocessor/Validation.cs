4f61ccf6d4be82f0d5d8d6113aba018fc42e4459
AHeroicLlama <AHeroicLlama@users.noreply.github.com>
2021-03-03 17:14:43 +0000

    Optimise Preprocessor
    In order to support exporting of all signatures, made the following changes:
    Preprocessor is now heavily parallelised - each file and each row is processed in parallel.
    Parallelisation has increased memory footprint.
    Converted to 64-bit building only, to allow enough mem alloc for larger exports.
    Ensured file rows are nulled once finished to keep memory usage tight.
    Refactored some validation RegExs into their own Class.

---
+++ Preprocessor/Validation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Mappalachia
+{
+	public static class Validation
+	{
+		public static readonly Regex headersToEscape = new Regex("[Ee]ditorID|[Dd]isplayName");
+		public static readonly Regex propertyWithoutFormID = new Regex("(.*) \\[.{4}:.{8}\\]");
+		public static readonly Regex componentNameOnly = new Regex(".* \"(.*)\" .*");
+		public static readonly Regex formIdOnly = new Regex("\\[.{4}:(.{8})\\]");
+		public static readonly Regex matchFormID = new Regex("[0-9A-F]{8}");
+		public static readonly Regex validNpcClass = new Regex("^(Main|Sub)$");
+		public static readonly Regex validPrimitiveShape = new Regex("^(Box|Sphere|Line|Plane|Ellipsoid)$");
+		public static readonly Regex validLockLevel = new Regex("^(Advanced \\(Level 1\\)|Chained|Expert \\(Level 2\\)|Inaccessible|Master \\(Level 3\\)|Novice \\(Level 0\\)|Opens Door|Requires Key|Requires Terminal|Unknown|Barred)$");
+		public static readonly Regex validSignature = new Regex("[A-Z_]{4}");
+		public static readonly Regex unescapedDoubleQuote = new Regex("(?<!\\\\)(\")");
+
+		public static readonly List<string> decimalHeaders = new List<string> { "x", "y", "boundX", "boundY" };
+	}
+}
