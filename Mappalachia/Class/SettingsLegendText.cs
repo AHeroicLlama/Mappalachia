524a035454922f08dec84f8d8bde87451968e45e
AHeroicLlama <tom-kellett@hotmail.co.uk>
2021-03-27 19:22:45 +0000

    Add as group checkbox, basics of override legend text from
    Adds a checkbox which allows us to toggle between adding under the same legend group and using a new one per item.
    Added the basic skeleton of a new Form and Settings to override legend text. (WIP)

---
+++ Mappalachia/Class/SettingsLegendText.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Mappalachia.Class
+{
+	//Stores settings for overridden legend text by legend group.
+	static class SettingsLegendText
+	{
+		public static Dictionary<int, PlotIcon> text = new Dictionary<int, PlotIcon>();
+	}
+}
