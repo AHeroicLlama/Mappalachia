e7d91815c8341f62f779bb8247adb1b84bca62c3
AHeroicLlama <tom-kellett@hotmail.co.uk>
2021-06-03 19:16:17 +0100

    Import and add Z coordinates to all MapDataPoints
    Includes z and Zbound coordinates for MapDataPoints, laying a foundation for a topography mode.
    Reworked and simplified ctors for MapDataPoint also.
    Z data is unused at this stage.

--- Mappalachia/SQL/getCoordsNPC.sql
+++ Mappalachia/SQL/getCoordsNPC.sql
@@ -1,3 +1,3 @@
-SELECT x, y, chance*100 AS chance FROM SeventySix_Worldspace
+SELECT x, y, z, chance*100 AS chance FROM SeventySix_Worldspace
 INNER JOIN SeventySix_NPCSpawn ON class = spawnClass AND SeventySix_Worldspace.locationFormID = SeventySix_NPCSpawn.locationFormID
 WHERE npc = $npc AND chance >= $minChance
