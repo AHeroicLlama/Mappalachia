f64727eb3a3e86de868ef72e1a94de1367bbab60
AHeroicLlama <tom-kellett@hotmail.co.uk>
2021-07-16 12:36:52 +0100

    Commenting, redundant imports, update SQLite

--- Mappalachia/Class/Notify.cs
+++ Mappalachia/Class/Notify.cs
@@ -1,11 +1,8 @@
-using System.Threading;
 using System.Windows.Forms;
 
 namespace Mappalachia.Class
 {
 	// Neatly packaged message boxes
-	/*This means messageBoxes indicating errors etc don't have to be handled by classes not responsible for UI
-	and allows ones called during work to not block said work if required*/
 	public static class Notify
 	{
 		public static void Info(string text)
