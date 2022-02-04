78c63f69b68ca2a303e0d52fd2d1bdb1bedd233e
AHeroicLlama <AHeroicLlama@users.noreply.github.com>
2022-02-04 16:23:52 +0000

    Merge Interior and Worldspace in Database, rename tables
    Merges the concepts of Interiors/Cells and the Worldspaces into a generic 'space' concept, allowing them to be handled the same. Still WIP this database will not work with Mappalachia at this point.

--- Database/sql/replaceEscapedChars.sql
+++ Database/sql/replaceEscapedChars.sql
@@ -1,8 +1,8 @@
-UPDATE SeventySix_FormId SET displayName = replace(displayName, ':COMMA:', ','); --Comma char was replaced by :COMMA: to keep both CSV and auto-generated SQL valid
-UPDATE SeventySix_FormId SET displayName = replace(displayName, '\"', '"'); --Quotation char needed escaping for the initial import to work, but can now be replaced
---There are 6 (') because each (') is escaped as ('') and each string is then wrapped in ('')
-UPDATE SeventySix_FormId SET displayName = replace(displayName, '''''', ''''); --Single quotes needed escaping as 2x singles for the initial import to work, but can now be replaced
+UPDATE Entity_Info SET displayName = replace(displayName, ':COMMA:', ','); -- Comma char was replaced by :COMMA: to keep both CSV and auto-generated SQL valid
+UPDATE Entity_Info SET displayName = replace(displayName, '\"', '"'); -- Quotation char needed escaping for the initial import to work, but can now be replaced
+-- There are 6 (') because each (') is escaped as ('') and each string is then wrapped in ('')
+UPDATE Entity_Info SET displayName = replace(displayName, '''''', ''''); -- Single quotes needed escaping as 2x singles for the initial import to work, but can now be replaced
 
-UPDATE SeventySix_Cell SET cellDisplayName = replace(cellDisplayName, ':COMMA:', ',');
-UPDATE SeventySix_Cell SET cellDisplayName = replace(cellDisplayName, '\"', '"');
-UPDATE SeventySix_Cell SET cellDisplayName = replace(cellDisplayName, '''''', '''');
+UPDATE Space_Info SET spaceDisplayName = replace(spaceDisplayName, ':COMMA:', ',');
+UPDATE Space_Info SET spaceDisplayName = replace(spaceDisplayName, '\"', '"');
+UPDATE Space_Info SET spaceDisplayName = replace(spaceDisplayName, '''''', '''');
