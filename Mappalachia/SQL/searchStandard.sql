139bef0f2609b624d150a87f147709a14ceff110
AHeroicLlama <tom-kellett@hotmail.co.uk>
2022-02-06 18:31:03 +0000

    Optimise search query

--- Mappalachia/SQL/searchStandard.sql
+++ Mappalachia/SQL/searchStandard.sql
@@ -1,26 +1,24 @@
-SELECT * FROM
-(
-	SELECT
-		displayName,
-		editorID,
-		signature,
-		COUNT(*) AS amount,
-		lockLevel,
-		entityFormID,
-		spaceDisplayName,
-		spaceEditorID,
-		Position_Data.spaceFormID as spaceFormID,
-		isWorldspace
-	FROM Entity_Info
+SELECT displayName,
+	editorID,
+	signature,
+	COUNT(*) AS amount,
+	lockLevel,
+	entityFormID,
+	spaceDisplayName,
+	spaceEditorID,
+	Position_Data.spaceFormID as spaceFormID,
+	isWorldspace
+FROM Entity_Info
 	INNER JOIN Position_Data ON referenceFormID = entityFormID
 	INNER JOIN Space_Info ON Space_Info.spaceFormID = Position_Data.spaceFormID
-	WHERE
-		(
-			(EditorId LIKE $searchTerm ESCAPE '\' OR
-			displayName LIKE $searchTerm ESCAPE '\' OR
-			entityFormID LIKE $searchTerm ESCAPE '\')
-		)
-	GROUP BY entityFormID, Space_Info.spaceFormID
-	ORDER BY isWorldspace DESC, amount DESC
-)
-WHERE spaceFormID = $spaceFormID
+WHERE
+	(
+		(EditorId LIKE $searchTerm ESCAPE '\' OR
+		displayName LIKE $searchTerm ESCAPE '\' OR
+		entityFormID LIKE $searchTerm ESCAPE '\')
+		AND Position_Data.spaceFormID = $spaceFormID
+		--AND signature IN ($allowedSignatures)
+		--AND lockLevel IN ($allowedLockTypes)
+	)
+GROUP BY entityFormID, Space_Info.spaceFormID
+ORDER BY isWorldspace DESC, amount DESC
