19c36ba5089b6bd5f24aaf5431f2c71b01108e1b
AHeroicLlama <AHeroicLlama@users.noreply.github.com>
2022-02-08 22:54:56 +0000

    Faster standard search query
    Still needs optimising

--- Mappalachia/SQL/searchStandard.sql
+++ Mappalachia/SQL/searchStandard.sql
@@ -1,24 +1,26 @@
-SELECT displayName,
-	editorID,
-	signature,
-	COUNT(*) AS amount,
-	lockLevel,
-	entityFormID,
-	spaceDisplayName,
-	spaceEditorID,
-	Position_Data.spaceFormID as spaceFormID,
-	isWorldspace
-FROM Entity_Info
-	INNER JOIN Position_Data ON referenceFormID = entityFormID
-	INNER JOIN Space_Info ON Space_Info.spaceFormID = Position_Data.spaceFormID
-WHERE
+SELECT displayName, editorId, signature, internalCount, lockLevel, entityFormID, spaceDisplayName, spaceEditorID, spaceFormID, isWorldspace FROM
+(
+	SELECT spaceFormId as internalSpaceFormId, referenceFormID, lockLevel, count(*) as internalCount FROM Position_Data WHERE
 	(
-		(EditorId LIKE $searchTerm ESCAPE '\' OR
-		displayName LIKE $searchTerm ESCAPE '\' OR
-		entityFormID LIKE $searchTerm ESCAPE '\')
-		AND Position_Data.spaceFormID = $spaceFormID
-		AND signature IN ($allowedSignatures)
-		AND lockLevel IN ($allowedLockTypes)
+		spaceFormID = '0025DA15'
+		AND lockLevel IN ('','Novice (Level 0)','Advanced (Level 1)','Expert (Level 2)','Master (Level 3)','Requires Terminal','Requires Key','Chained','Opens Door','Inaccessible','Unknown')
 	)
-GROUP BY entityFormID, Space_Info.spaceFormID
-ORDER BY isWorldspace DESC, amount DESC
+	GROUP BY referenceFormID, spaceFormID
+)
+
+INNER JOIN
+
+(
+	SELECT * FROM Entity_Info WHERE
+	(
+		(EditorId LIKE '%a%' ESCAPE '\' OR
+		displayName LIKE '%a%' ESCAPE '\' OR
+		entityFormID LIKE '%a%' ESCAPE '\') AND
+		signature IN ('STAT','LVLI','FLOR','NPC_','MISC','BOOK','ALCH','CONT','FURN','NOTE','TERM','ARMO','WEAP','AMMO','PROJ','CNCY','KEYM','ACTI')
+	)
+)
+ON referenceFormID = entityFormID
+
+INNER JOIN Space_Info ON spaceFormID = internalSpaceFormId
+
+ORDER BY isWorldspace DESC, internalCount DESC
