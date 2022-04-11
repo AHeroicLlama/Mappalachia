993ad5ffcf4ec51f6a89d80df11807c4bc6fe5f1
AHeroicLlama <tom-kellett@hotmail.co.uk>
2022-04-11 15:19:54 +0100

    Shave 70MB from DB file by removing Appalachia FormID
    Replaces millions of repeated instances of Appalachia Worldspace ID for empty string, saving approx 17% from the final database file size. This trick only really works once before getting messy, and as this field is not targeted by code nor user-facing. Due to the approach this does not reduce the file size of the final compressed release.

---
+++ Database/sql/compress.sql
@@ -0,0 +1,7 @@
+-- Special case to remove repeated instances of Appalachia worldspace ID to reduce DB file size
+UPDATE Map_Markers SET spaceFormID = '' WHERE spaceFormID = '0025DA15';
+UPDATE NPC_Search SET spaceFormID = '' WHERE spaceFormID = '0025DA15';
+UPDATE Scrap_Search SET spaceFormID = '' WHERE spaceFormID = '0025DA15';
+UPDATE Standard_Search SET spaceFormID = '' WHERE spaceFormID = '0025DA15';
+UPDATE Position_Data SET spaceFormID = '' WHERE spaceFormID = '0025DA15';
+UPDATE Space_Info SET spaceFormID = '' WHERE spaceFormID = '0025DA15';
