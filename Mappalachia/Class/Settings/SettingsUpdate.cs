4dff3e5abae7304f85dfbe6cd97a639badb6aa5f
AHeroicLlama <AHeroicLlama@users.noreply.github.com>
2022-10-18 19:09:57 +0100

    Change update prompt cooldown to 24h

--- Mappalachia/Class/Settings/SettingsUpdate.cs
+++ Mappalachia/Class/Settings/SettingsUpdate.cs
@@ -5,7 +5,7 @@ namespace Mappalachia.Class
 	// Settings related to the Update Checker
 	class SettingsUpdate
 	{
-		static readonly TimeSpan updatePromptCooldownPeriod = TimeSpan.FromHours(12);
+		static readonly TimeSpan updatePromptCooldownPeriod = TimeSpan.FromHours(24);
 		public static DateTime lastDeclinedUpdate = DateTime.UnixEpoch;
 
 		public static TimeSpan GetCooldownPeriod()
