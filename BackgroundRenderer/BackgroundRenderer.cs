bcab5034d778fa4322fcf813bacdf135823dc255
AHeroicLlama <AHeroicLlama@users.noreply.github.com>
2024-10-26 00:06:32 +0100

    WIP BG Renderer
    Provides a process where the user can draw a bounding box around a cell which is (by default) too zoomed out and/or not centered, and it will be corrected for.

--- BackgroundRenderer/BackgroundRenderer.cs
+++ BackgroundRenderer/BackgroundRenderer.cs
@@ -16,7 +16,7 @@ namespace BackgroundRenderer
 			SqliteConnection connection = BuildTools.GetNewConnection();
 
 			BuildTools.StdOutWithColor("Enter a space-separated list of space EditorIDs to render. Or enter nothing to render all", BuildTools.QuestionColor);
-			string input = "TheWayward01";// Console.ReadLine() ?? string.Empty;
+			string input = "DuncanDuncanRobotics01";// Console.ReadLine() ?? string.Empty;
 
 			Stopwatch stopwatch = Stopwatch.StartNew();
 
@@ -42,24 +42,35 @@ namespace BackgroundRenderer
 		{
 			double resolution = Misc.MapImageResolution;
 
-			double scale = space.MaxRange / resolution;
+			double scale = resolution / space.MaxRange;
 			double cameraX = space.CenterX;
 			double cameraY = space.CenterY;
 			double cameraZ = BuildTools.CroppedHeights.TryGetValue(space.EditorID, out int cropppedHeight) ? cropppedHeight : (int)Math.Pow(2, 16);
 			string outputFileName = "temp_out.dds";
 
-			if (false)
+			if (true)
 			{
-				int topLeftX = 534;
-				int topLeftY = 830;
-				int width = 1728;
-				int height = 1941;
+				int width = 1178;
+				int height = 1184;
+
+				int topLeftX = 1943;
+				int topLeftY = 984;
+
+				double centerX = topLeftX + (width / 2d); // correct
+				double centerY = topLeftY + (height / 2d); // correct
+
+				double pxCorrectionX = (resolution / 2d) - centerX; // correct
+				double pcCorrectionY = (resolution / 2d) - centerY; // correct
+
+				double actualCorrectionX = pxCorrectionX / scale;
+				double actualCorrectionY = pcCorrectionY / scale;
 
 				scale *= resolution / Math.Max(width, height);
-				cameraX = space.CenterX - (resolution / 2d) + (width / 2d) + topLeftX;
-				cameraY = space.CenterY + (resolution / 2d) - (height / 2d) - topLeftY;
-			}
+				cameraX -= actualCorrectionX;
+				cameraY += actualCorrectionY;
 
+				Console.WriteLine(scale);
+			}
 
 			string renderCommand = $"{BuildTools.Fo76UtilsRenderPath} \"{BuildTools.GameESMPath}\" {outputFileName} {resolution} {resolution} " +
 				$"\"{BuildTools.GameDataPath.WithoutTrailingSlash()}\" {(space.IsWorldspace ? $"-btd \"{BuildTools.GameTerrainPath}\"" : string.Empty)} -w 0x{space.FormID.ToHex()} -l 0 -cam {scale} 180 0 0 {cameraX} {cameraY} {cameraZ} " +
