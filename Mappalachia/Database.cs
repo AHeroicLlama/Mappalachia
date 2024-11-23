7b72dbf833aa3114d21b9740e8858946f2309968
AHeroicLlama <AHeroicLlama@users.noreply.github.com>
2024-11-23 21:34:17 +0000

    Move reader extensions

--- Mappalachia/Database.cs
+++ Mappalachia/Database.cs
@@ -1,5 +1,4 @@
-﻿using System.Data;
-using Library;
+﻿using Library;
 using Microsoft.Data.Sqlite;
 using static Library.CommonDatabase;
 
@@ -9,7 +8,7 @@ namespace Mappalachia
 	{
 		public static SqliteConnection Connection { get; } = GetNewConnection(Paths.DatabasePath);
 
-		public static List<Space> Spaces { get; } = GetSpaces(Connection); // TODO - somewhere to store/cache these
+		public static List<Space> Spaces { get; } = GetSpaces(Connection);
 
 		public static List<MapMarker> MapMarkers { get; } = GetMapMarkers(Connection);
 
@@ -37,76 +36,6 @@ namespace Mappalachia
 			}
 
 			return instances;
-		}
-
-		// TODO move reader extensions
-		static Coord GetCoord(this SqliteDataReader reader)
-		{
-			return new Coord(
-				reader.GetFloat(reader.GetOrdinal("x")),
-				reader.GetFloat(reader.GetOrdinal("y")),
-				reader.GetFloat(reader.GetOrdinal("z")));
-		}
-
-		// Returns an optional float from the column name of the reader.
-		// Given a null or empty, returns 0
-		static float GetFloat(this SqliteDataReader reader, string columnName)
-		{
-			int ordinal = reader.GetOrdinal(columnName);
-
-			if (reader.IsDBNull(ordinal) || string.IsNullOrEmpty(reader.GetString(ordinal)))
-			{
-				return 0f;
-			}
-
-			return reader.GetFloat(ordinal);
-		}
-
-		static uint GetUInt(this SqliteDataReader reader, string columnName)
-		{
-			return (uint)reader.GetInt32(reader.GetOrdinal(columnName));
-		}
-
-		static string GetString(this SqliteDataReader reader, string columnName)
-		{
-			return reader.GetString(reader.GetOrdinal(columnName));
-		}
-
-		// Returns the Space with the FormID present in the reader with the given column
-		static Space? GetSpace(this SqliteDataReader reader, string columnName)
-		{
-			int ordinal = reader.GetOrdinal(columnName);
-
-			if (string.IsNullOrEmpty(reader.GetString(ordinal)))
-			{
-				return null;
-			}
-
-			return Spaces.Where(space => space.FormID == reader.GetUInt(columnName)).FirstOrDefault();
-		}
-
-		static LockLevel GetLockLevel(this SqliteDataReader reader)
-		{
-			int ordinal = reader.GetOrdinal("lockLevel");
-
-			if (reader.IsDBNull(ordinal) || string.IsNullOrEmpty(reader.GetString(ordinal)))
-			{
-				return LockLevel.None;
-			}
-
-			return Enum.Parse<LockLevel>(reader.GetString(ordinal).WithoutWhitespace());
-		}
-
-		static Shape? GetShape(this SqliteDataReader reader)
-		{
-			int shapeOrdinal = reader.GetOrdinal("primitiveShape");
-
-			if (reader.IsDBNull(shapeOrdinal) || string.IsNullOrEmpty(reader.GetString(shapeOrdinal)))
-			{
-				return null;
-			}
-
-			return new Shape(Enum.Parse<ShapeType>(reader.GetString(shapeOrdinal)), reader.GetFloat("boundX"), reader.GetFloat("boundY"), reader.GetFloat("boundZ"), reader.GetFloat("rotZ"));
 		}
 	}
 }
---
+++ Mappalachia/ReaderExtensions.cs
@@ -0,0 +1,78 @@
+﻿using Library;
+using Microsoft.Data.Sqlite;
+
+namespace Mappalachia
+{
+	// Extension methods for SqliteDataReader
+	public static class ReaderExtensions
+	{
+		public static Coord GetCoord(this SqliteDataReader reader)
+		{
+			return new Coord(
+				reader.GetFloat(reader.GetOrdinal("x")),
+				reader.GetFloat(reader.GetOrdinal("y")),
+				reader.GetFloat(reader.GetOrdinal("z")));
+		}
+
+		// Returns an optional float from the column name of the reader.
+		// Given a null or empty, returns 0
+		public static float GetFloat(this SqliteDataReader reader, string columnName)
+		{
+			int ordinal = reader.GetOrdinal(columnName);
+
+			if (reader.IsDBNull(ordinal) || string.IsNullOrEmpty(reader.GetString(ordinal)))
+			{
+				return 0f;
+			}
+
+			return reader.GetFloat(ordinal);
+		}
+
+		public static uint GetUInt(this SqliteDataReader reader, string columnName)
+		{
+			return (uint)reader.GetInt32(reader.GetOrdinal(columnName));
+		}
+
+		public static string GetString(this SqliteDataReader reader, string columnName)
+		{
+			return reader.GetString(reader.GetOrdinal(columnName));
+		}
+
+		// Returns the Space with the FormID present in the reader with the given column
+		public static Space? GetSpace(this SqliteDataReader reader, string columnName)
+		{
+			int ordinal = reader.GetOrdinal(columnName);
+
+			if (string.IsNullOrEmpty(reader.GetString(ordinal)))
+			{
+				return null;
+			}
+
+			return Database.Spaces.Where(space => space.FormID == reader.GetUInt(columnName)).FirstOrDefault();
+		}
+
+		public static LockLevel GetLockLevel(this SqliteDataReader reader)
+		{
+			int ordinal = reader.GetOrdinal("lockLevel");
+
+			if (reader.IsDBNull(ordinal) || string.IsNullOrEmpty(reader.GetString(ordinal)))
+			{
+				return LockLevel.None;
+			}
+
+			return Enum.Parse<LockLevel>(reader.GetString(ordinal).WithoutWhitespace());
+		}
+
+		public static Shape? GetShape(this SqliteDataReader reader)
+		{
+			int shapeOrdinal = reader.GetOrdinal("primitiveShape");
+
+			if (reader.IsDBNull(shapeOrdinal) || string.IsNullOrEmpty(reader.GetString(shapeOrdinal)))
+			{
+				return null;
+			}
+
+			return new Shape(Enum.Parse<ShapeType>(reader.GetString(shapeOrdinal)), reader.GetFloat("boundX"), reader.GetFloat("boundY"), reader.GetFloat("boundZ"), reader.GetFloat("rotZ"));
+		}
+	}
+}
